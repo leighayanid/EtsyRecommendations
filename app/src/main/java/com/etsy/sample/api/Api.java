4d3a6e588a3327edde0599fa472a19a1b6535432
Leigh R. Dinaya <leighdinaya04@gmail.com>
2016-03-16 09:33:12 +0800

    Added interface class and method for api call.

---
+++ app/src/main/java/com/etsy/sample/api/Api.java
@@ -0,0 +1,19 @@
+package com.etsy.sample.api;
+
+import com.etsy.sample.model.ActiveListings;
+
+import retrofit.Callback;
+import retrofit.http.GET;
+import retrofit.http.Query;
+
+/**
+ * Created by user on 3/16/2016.
+ */
+public interface Api {
+
+    //method for api call
+    //GET is the protocol, uri is listings/active which can be found on etsy documentation
+    @GET("listings/active")
+    void activeListings(@Query("includes") String includes, Callback<ActiveListings> callback); //to include optional parameters and annotate
+
+}
--- app/src/main/java/com/etsy/sample/api/Etsy.java
+++ app/src/main/java/com/etsy/sample/api/Etsy.java
@@ -1,8 +1,29 @@
 package com.etsy.sample.api;
 
+import retrofit.RequestInterceptor;
+import retrofit.RestAdapter;
+
 /**
  * Created by user on 1/26/2016.
  */
 public class Etsy {
-    public static final String APP_KEY = "i4yhhu9xoxsvidnkwmbsz308";
+    public static final String API_KEY = "i4yhhu9xoxsvidnkwmbsz308";
+
+    private static RequestInterceptor getInterceptor(){
+        return new RequestInterceptor() {
+            @Override
+            public void intercept(RequestFacade request) {
+                request.addEncodedQueryParam("api_key", API_KEY);
+            }
+        };
+    }
+
+    //method to retrieve back an instance of api interface
+    private static Api getApi(){
+        return new RestAdapter.Builder()
+                .setEndpoint("https://openapi.etsy.com/v2")
+                .setRequestInterceptor(getInterceptor())
+                .build()
+                .create(Api.class);
+    }
 }
