6facac30c6dae2f9b5cb0e32deb149c3c775b1c6
Leigh R. Dinaya <leighdinaya04@gmail.com>
2016-03-18 07:34:54 +0800

    Added adapter class that will bind data to recyclerview

---
+++ app/src/main/java/com/etsy/sample/adapter/ListingAdapter.java
@@ -0,0 +1,86 @@
+package com.etsy.sample.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.etsy.sample.R;
+import com.etsy.sample.model.ActiveListings;
+import com.etsy.sample.model.Listing;
+import com.squareup.picasso.Picasso;
+
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+
+/**
+ * Created by user on 3/18/2016.
+ */
+public class ListingAdapter extends RecyclerView.Adapter<ListingAdapter.ListingHolder> implements Callback<ActiveListings> {
+
+    private LayoutInflater inflater; //inflates layout listing xml to recyclerview
+    private ActiveListings activeListings;
+
+    //constructor. need context for the layout inflater
+    public ListingAdapter(Context context) {
+        inflater = LayoutInflater.from(context);
+
+    }
+
+    @Override
+    public ListingHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new ListingHolder(inflater.inflate(R.layout.layout_listing, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(ListingHolder holder, int position) {
+        final Listing listing = activeListings.results[position];
+        holder.mTitle.setText(listing.title);
+        holder.mShopName.setText(listing.Shop.shop_name);
+        holder.mPrice.setText(listing.price);
+        Picasso.with(holder.mImageview.getContext())
+                .load(listing.Images[0].url_570xN)
+                .into(holder.mImageview);
+    }
+
+    @Override
+    public int getItemCount() {
+        if (activeListings == null)
+            return 0;
+        if (activeListings.results == null)
+            return 0;
+
+        return activeListings.results.length;
+    }
+
+    @Override
+    public void success(ActiveListings activeListings, Response response) {
+        this.activeListings = activeListings;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public void failure(RetrofitError error) {
+
+    }
+
+    public class ListingHolder extends RecyclerView.ViewHolder {
+
+        public ImageView mImageview;
+        public TextView mTitle;
+        public TextView mShopName;
+        public TextView mPrice;
+
+        public ListingHolder(View itemView) {
+            super(itemView);
+            mImageview = (ImageView) itemView.findViewById(R.id.listing_image);
+            mTitle = (TextView) itemView.findViewById(R.id.listing_title);
+            mShopName = (TextView) itemView.findViewById(R.id.listing_shop_name);
+            mPrice = (TextView) itemView.findViewById(R.id.listing_price);
+        }
+    }
+}
